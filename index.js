69673d1c47df123e153b0f5f92a15d50ea7dd42b
Hosein1380 <ghafary.tm37@gmail.com>
2022-03-12 11:24:04 +0330

    localstorage

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 "use srict";
+function registerUser(){
+    let fname= document.getElementById('fname').value;
+    let lname= document.getElementById('lname').value;
+    let email= document.getElementById('email').value;
+    let pass= document.getElementById('pass').value;
+    localStorage.setItem('fname',fname);
+    localStorage.setItem('lname',lname);
+    localStorage.setItem('email',email);
+    localStorage.setItem('pass',pass);
+}
+
+
 
